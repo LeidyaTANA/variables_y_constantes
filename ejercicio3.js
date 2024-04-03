48091f04f3d1faf1104e8e4822a3b313d60971a2
leidy <yurleidyecheverriatanaka@gamil.com>
2024-04-02 20:25:17 -0500

    add ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+//ejercicio2
+/*Defina una variable llamada nombre y asigne su nombre
+Debe imprimir en la consola Hola , seguido del nombre y un signo de
+exclamación. Debe usar template literals .
+Por ejemplo si el usuario ingresa "Pedro" el programa debe imprimir en la
+consola:*/
+
+let name = "leidy";
+console.log("hola," ,name,"!");
