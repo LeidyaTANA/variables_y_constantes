312822734694de06a6fe0c5b325c40916e7196ff
leidy <yurleidyecheverriatanaka@gamil.com>
2024-04-02 20:51:02 -0500

    add ejercicio4

---
+++ ejercicio5.js
@@ -0,0 +1,12 @@
+//ejercicio5
+/*El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor
+que determina la cantidad de grasa de una persona.
+El BMI se calcula con la siguiente formula:
+peso / altura**2 = peso / altura al cuadrado
+Declara y asigna las variables peso y altura para calcular el IMC e imprima la
+frase */
+
+let peso = 70;
+let altura = 1.68;
+let IMC = peso/altura**2;
+console.log("Tú IMC es", IMC);
